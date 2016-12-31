0d437a5390270b22467818face119c942eb4f5f6
Glen Little <glen.little@outlook.com>
2016-12-31 18:44:25 -0500

    working on listing

--- src/pages/Listing.vue
+++ src/pages/Listing.vue
@@ -5,13 +5,17 @@
       <option value="HS">Holy Days</option>
       <option value="M">Months</option>
     </select>
-    <div v-for="item in filteredList">
+    <!--<div v-for="item in filteredList">
       {{item.Type}} {{item.NameEn}} <span v-html="item.NameAr"></span> {{item.BMonthDay.d}}
-    </div>
+    </div>-->
+    <q-data-table :data="filteredList" :config="config" :columns="columns">
+    </q-data-table>
   </article>
 </template>
 <script>
   import badi from '../scripts/badiCalc'
+  var cloneDeep = require('lodash/cloneDeep');
+
   export default {
     name: 'listing', // for Vue debugger
     data() {
@@ -20,7 +24,110 @@
         icon: 'people',
         listFilter: ['HS'],
         num: 0,
-        list: []
+        list: [],
+        config: {
+          // [REQUIRED] Set the row height
+          rowHeight: '50px',
+          // (optional) Title to display
+          // title: this.title,
+          // (optional) Display refresh button
+          // refresh: true,
+          // (optional)
+          // User will be able to choose which columns
+          // should be displayed
+          // columnPicker: true,
+          // (optional) How many columns from the left are sticky
+          // leftStickyColumns: 0,
+          // (optional) How many columns from the right are sticky
+          // rightStickyColumns: 2,
+          // (optional)
+          // Styling the body of the data table;
+          // "minHeight", "maxHeight" or "height" are important
+          bodyStyle: {
+            maxHeight: '500px'
+          },
+          // (optional) By default, Data Table is responsive,
+          // but you can disable this by setting the property to "false"
+          //responsive: true,
+          // (optional) Use pagination. Set how many rows per page
+          // and also specify an additional optional parameter ("options")
+          // which forces user to make a selection of how many rows per
+          // page he wants from a specific list
+          // pagination: {
+          //   rowsPerPage: 15,
+          //   options: [5, 10, 15, 30, 50, 500]
+          // },
+          // (optional) User can make selections. When "single" is specified
+          // then user will be able to select only one row at a time.
+          // Otherwise use "multiple".
+          selection: false, //'multiple',
+          // (optional) Override default messages when no data is available
+          // or the user filtering returned no results.
+          messages: {
+            noData: '<i>warning</i> No data available to show.',
+            noDataAfterFiltering: '<i>warning</i> No results. Please refine your search terms.'
+          }
+        },
+        columns: [{
+          label: 'Day',
+          field: '',
+          width: '',
+          classes: ''
+        }, {
+          label: 'Name',
+          field: 'NameEn',
+          format(value) {
+            switch (value) {
+              case 'M':
+                return "Feast"
+              case 'HS':
+              case 'HD':
+                return "Holy Day"
+              default:
+                return value;
+            }
+          },
+          width: '80px'
+        }, {
+          label: 'Suspend Work',
+          field: '',
+          width: '',
+          classes: ''
+        }, {
+          label: 'Meeting/Special Time',
+          field: '',
+          width: '',
+          classes: ''
+        }, {
+          label: 'Start of Day',
+          field: '',
+          width: '',
+          classes: ''
+        }, {
+          label: 'Type',
+          field: 'Type',
+          width: '20px',
+          filter: true,
+          sort: true,
+          format(value) {
+            switch (value) {
+              case 'M':
+                return "Feast"
+              case 'HS':
+              case 'HD':
+                return "Holy Day"
+              default:
+                return value;
+            }
+          }
+        }, {
+          label: 'What',
+          field: 'BMonthDay',
+          format(value) {
+            return value.d
+          },
+          width: '80px'
+        }]
       }
     },
     created: function () {
@@ -34,11 +141,12 @@
     methods: {
       getDateInfos: function (year) {
         var info = badi.prepareDateInfos(year);
+        window.dis = cloneDeep(info);
         this.num = info.length;
         this.list = info;
       }
     },
-        head: {
+    head: {
       title: function () {
         return {
           inner: this.title
