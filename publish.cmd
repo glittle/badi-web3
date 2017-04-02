1bc247e748c7ae0b3e8a77b05e48bb72af18477e
Glen Little <glen.little@gmail.com>
2017-04-02 11:44:19 -0400

    fix script

--- publish.cmd
+++ publish.cmd
@@ -1,5 +1,5 @@
 robocopy c:\dev\badi-web3-live c:\dev\badi-web3-backup /mir 
-npm version patch
-quasar build
+call npm version patch
+call quasar build
 robocopy c:\dev\badi-web3\dist c:\dev\badi-web3-live /mir 
 
